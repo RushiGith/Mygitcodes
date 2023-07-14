170cc9925cd80e21a39f66ecc023890fd7988e81
rushi <rushikeshgomase2@gmail.com>
2023-07-14 18:26:40 +0530

    First task

---
+++ src/com/velocity/Arrya/Arrya.java
@@ -0,0 +1,19 @@
+package com.velocity.Arrya;
+
+import java.util.ArrayList;
+
+public class Arrya {
+
+	public static void main(String[] args) {
+		ArrayList<String> al= new ArrayList<String>();
+		al.add("Rushi");
+		al.add("Vipul");
+		al.add("Atharva");
+		for(String x:al)
+		{
+			System.out.println(x);
+		}
+
+	}
+
+}
